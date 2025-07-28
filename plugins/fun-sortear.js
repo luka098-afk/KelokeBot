7b7be4eb542a0a2bce5d21217dd66a7fa3cf1c94
luka098-afk <krebskrebs17@gmail.com>
2025-07-28 13:26:53 -0300

    Agregado nuevo comando

--- plugins/fun-sortear.js
+++ plugins/fun-sortear.js
@@ -1,36 +1,47 @@
 import util from 'util'
 import path from 'path'
 
-async function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+async function handler(m, { groupMetadata, conn, text, usedPrefix }) {
+  const user = jid => '@' + jid.split('@')[0]
 
-let user = a => '@' + a.split('@')[0]
-if (!text) return conn.reply(m.chat, `${emoji} Por favor ingresa lo que deseas sortear.`, m)
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let k = Math.floor(Math.random() * 70)
-let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*[🥳 \`ＦＥＬＩＣＩＤＡＤＥＳ\` 🥳]*\n\n${user(a)} 🥳\nAcaba de ganar el sorteo felicitaciones 🎉`
-let txt = ''
-let count = 0
-for (const c of top) {
-await new Promise(resolve => setTimeout(resolve, 15))
-txt += c
-count++
+  if (!text) return conn.reply(m.chat, `🕷️ Por favor escribe lo que deseas sortear.\n\nEjemplo:\n${usedPrefix}sortear una skin, un rol, etc.`, m)
 
-if (count % 10 === 0) {
-conn.sendPresenceUpdate('composing' , m.chat);
-}
-}
-await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
+  let participantes = groupMetadata.participants.map(v => v.id)
+  let ganador = participantes[Math.floor(Math.random() * participantes.length)]
 
+  let audioIndex = Math.floor(Math.random() * 70)
+  let audioUrl = `https://hansxd.nasihosting.com/sound/sound${audioIndex}.mp3`
+
+  let mensaje = `🎉 *¡SORTEO AL AZAR!* 🎉\n\n📦 Premio: *${text}*\n🥳 Ganador: ${user(ganador)}\n\n¡Felicitaciones! 🎊`
+
+  // Animación de escritura
+  let txt = ''
+  let count = 0
+  for (const c of mensaje) {
+    await new Promise(resolve => setTimeout(resolve, 15))
+    txt += c
+    count++
+    if (count % 10 === 0) {
+      conn.sendPresenceUpdate('composing', m.chat)
+    }
+  }
+
+  await conn.sendMessage(m.chat, {
+    text: txt.trim(),
+    mentions: [ganador]
+  }, {
+    quoted: m,
+    ephemeralExpiration: 24 * 60 * 1000,
+    disappearingMessagesInChat: 24 * 60 * 1000
+  })
+
+  await conn.sendFile(m.chat, audioUrl, 'sorteo.mp3', null, m)
 }
-handler.help = ['sorteo']
-handler.command = ['sorteo']
+
+handler.help = ['sortear']
+handler.command = ['sortear']
 handler.tags = ['fun']
 handler.group = true
 handler.register = true
 
 export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
