f04481b3a3c7d6854a22d307b7e5c00e15f1e30f
luka098 <krebskrebs17@gmail.com>
2025-08-02 21:42:52 -0300

    nuevo

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -66,7 +66,7 @@ export async function before(m, { conn }) {
           description: null,
           previewType: "PHOTO",
           thumbnailUrl: 'http://imgfz.com/i/ysZD3vi.jpeg',
-          sourceUrl: 'https://whatsapp.com/channel/0029VawwvsW7j6g1upS0i531',
+          sourceUrl: '-',
           mediaType: 1,
           renderLargerThumbnail: true
         }
