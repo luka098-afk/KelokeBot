1b15925fef1646db45d631e49e2674b5b87b8096
luka098 <krebskrebs17@gmail.com>
2025-08-13 12:26:41 -0300

    Remove node_modules from repo

--- node_modules/@whiskeysockets/baileys/lib/Socket/index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-const Defaults_1 = require("../Defaults")
-const business_1 = require("./business")
-const makeWASocket = (config) => (business_1.makeBusinessSocket({
-...Defaults_1.DEFAULT_CONNECTION_CONFIG,
-...config
-}))
-exports.default = makeWASocket
